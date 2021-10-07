0b2ff7924c58a0aa6998211f3733c3365724c180
Vinanddrinks <vinanddrinks1@gmail.com>
2021-10-07 11:20:09 +0200

    initial commit for structure and kikstart the project

--- bakery.h
+++ bakery.h
@@ -4,5 +4,35 @@
 
 #ifndef BAKERY_BAKERY_H
 #define BAKERY_BAKERY_H
+//structures def start
+typedef struct Element_str{
+    char text[50];
+    struct Element_str* next;
+}Element_str;
+typedef struct Taste_Stack{
+    //Todo
+}Taste_Stack;
+typedef struct Cake{
+    Element_str* order;
+    Taste_Stack* s_tastes;
+}Cake;
+typedef struct Element_cake{
+    Cake* cake;
+    struct Element_str* next;
+}Element_cake;
+typedef struct Order_Queue{
+    //Todo
+}Order_Queue;
+typedef struct Tasting_Queue{
+    //Todo
+}Tasting_Queue;
 
+//str def end
+//start debug var
+int allocs;
+//end debug var
+
+// Start Prototype déclaration
+
+// End Prototype déclaration
 #endif //BAKERY_BAKERY_H
--- main.c
+++ main.c
@@ -1,4 +1,11 @@
 //
-// Created by Vinanddrinks on 07/10/2021.
+// Created by Vinanddrinks,Riccardis and Djay on 07/10/2021.
 //
-
+#include "stdlib.h"
+#include "stdio.h"
+#include "bakery.h"
+int main(){
+    allocs = 0;
+    printf("current dynamic memory allocated : %d \n",allocs);
+    return 0;
+}
